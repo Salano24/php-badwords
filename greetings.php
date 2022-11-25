78756f4866037991abd49fb5b8023f0a150c893c
Salano24 <salernomarco48@email.com>
2022-11-25 15:00:03 +0100

    fine esercizio

--- greetings.php
+++ greetings.php
@@ -1,3 +1,33 @@
 <?php
+$paragrafo = $_GET['paragrafo'];
+$censura = $_GET['censura'];
+
+
+
+$newstring = str_ireplace($censura, "***", $paragrafo) ;
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+    <link rel="stylesheet" href="style.scss">
+</head>
+<body>
+    <div class="container_result">
+        
+        <h1 class="string"> <?php echo $paragrafo."<br>";?> </h1>
+        <h2 class="lunghezza"> <?php echo "Il seguente paragrafo è lungo " . strlen($paragrafo) ." caratteri"; ?> </h2>
+
+        <br>
+
+        <h1 class="string"> <?php echo $newstring ."<br>";?> </h1>
+        <h2 class="lunghezza"> <?php echo "Il nuovo paragrafo è lungo " . strlen($newstring) ." caratteri"; ?> </h2>
+
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+    <link rel="stylesheet" href="style.scss">
+</head>
+<body>
+    <div class="container">
+        <form action="greetings.php" method="get">
+
+        <div >
+            <label for="paragrafo">Testo</label>
+            <input type="text" name="paragrafo" id="paragrafo" placeholder="Inserisci il tuo testo...">
+        </div>
+       <div>
+         <label for="censura">Censura</label>
+        <input type="text" name="censura" id="censura" placeholder="Censura...">
+       </div> 
+        <div class="button">
+           <button type="submit">invio</button>
+        <button type="reset">reset</button> 
+        </div>
+        
+    </div>
+   
+       
+      
+    </form> 
+</body>
+</html>
